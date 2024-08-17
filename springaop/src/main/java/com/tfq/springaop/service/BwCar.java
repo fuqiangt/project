3b9785f8687ff6f4af7b6a99bd2a9e017befb757
tangfq <65206596+fuqiangt@users.noreply.github.com>
2024-08-17 12:51:21 +0800

    Update BwCar.java

--- springaop/src/main/java/com/tfq/springaop/service/BwCar.java
+++ springaop/src/main/java/com/tfq/springaop/service/BwCar.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 
 /**
  *
- *  @description 宝马汽车实现Car接口
+ *  @description 宝马汽车实现Car接口,注入可以使用Lombok的全参构造器方法实现。
  *  @author tangfq; 
  *  @version 2020年5月24日 下午12:24:26
  *
